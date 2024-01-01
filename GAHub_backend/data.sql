666d182338f122a45ebf96ee02af796ad4171c34
ghbouzrbay <atighmarcelline@gmail.com>
2024-01-01 07:26:51 -0800

    update

---
+++ GAHub_backend/data.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS usersDB;
+
+USE usersDB;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
